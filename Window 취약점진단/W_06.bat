b06a3a1ca54f2db6e2d0293d3e0bbb9110e1be55
Min <112598226+jixxxxmin@users.noreply.github.com>
2025-07-14 19:09:26 +0900

    Update W_06.bat

--- Window 취약점진단/W_06.bat
+++ Window 취약점진단/W_06.bat
@@ -6,9 +6,7 @@ set /p ADgroup= 관리자 계정명을 입력하시오:
 cls
 
 for /f "skip=6 tokens=*" %%a in ('net localgroup %ADgroup% ^| findstr /v "명령을"') do (
-	for %%b in (%%a) do (
-		echo %%b
-	)
+	echo %%a
 )
 
 pause
